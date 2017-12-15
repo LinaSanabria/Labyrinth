0ddc72d7aea06818c9a2722396d3e31704a83484
Cristhian Andres Chamorro <32948119+cris2014971130@users.noreply.github.com>
2017-12-15 08:47:45 -0500

    Update Edge.java

--- src/com/uptc/prg3/datastruct/Edge.java
+++ src/com/uptc/prg3/datastruct/Edge.java
@@ -1,11 +1,30 @@
 package com.uptc.prg3.datastruct;
 
-public class Edge <V,E> {
-   protected E label;
-   protected Vertex <V,E> target;
 
-    public Edge(E label, Vertex<V, E> target) {
+public class Edge <T,L> {
+	
+   protected L label;
+   protected Vertex <T,L> target;
+
+    public Edge(L label, Vertex<T, L> target) {
         this.label = label;
         this.target = target;
     }
+    
+    public Vertex<T, L> getTarget() {
+		return target;
+	}
+
+	public void setTarget(Vertex<T, L> target) {
+		this.target = target;
+	}
+
+	
+	public boolean isEmpty(){
+		if(target == null){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
