cb6b8274d8df52b649997f9bb485be303848afa5
Cristhian Andres Chamorro <32948119+cris2014971130@users.noreply.github.com>
2017-12-15 08:46:06 -0500

    Update Vertex.java

--- src/com/uptc/prg3/datastruct/Vertex.java
+++ src/com/uptc/prg3/datastruct/Vertex.java
@@ -1,12 +1,37 @@
 package com.uptc.prg3.datastruct;
 
-public class Vertex <V,A> {
-    protected V info;
-    protected SimpleList <Edge<V,A>> edges;
 
-    public Vertex(V info) {
+public class Vertex <T,L> {
+    protected T info;
+    protected SimpleList <Edge<T,L>> edges;
+
+    public Vertex(T info) {
         this.info = info;
         this.edges = new SimpleList<>();
     }
+    
+    public void add(Edge<T, L> info){
+		edges.add(info);
+	}
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public SimpleList<Edge<T, L>> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(SimpleList<Edge<T, L>> edges) {
+		this.edges = edges;
+	}
+
+	public boolean isEmptyHead(){
+		return edges.isEmpty();
+	}
   
 }
